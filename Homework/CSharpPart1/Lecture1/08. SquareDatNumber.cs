b5d344dc2e884ec026e64858419e683dce7fcde6
georgiwe <georgiwe@gmail.com>
2013-11-03 16:00:31 +0200

    Create 08. SquareDatNumber.cs

---
+++ Homework/CSharpPart1/Lecture1/08. SquareDatNumber.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+class SquareDat
+{
+    static void Main()
+    {
+        //Create a console application that calculates and prints the square of the number 12345.
+
+        Console.WriteLine("12345 squared is {0}\n",12345*12345);
+    }
+}
