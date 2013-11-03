6fdf630f3f2d4e1e388f05e4acfefd5c4e70ac65
georgiwe <georgiwe@gmail.com>
2013-11-04 00:57:51 +0200

    Update 14. BankAccount.cs

--- Homework/CSharpPart1/Lecture2/14. BankAccount.cs
+++ Homework/CSharpPart1/Lecture2/14. BankAccount.cs
@@ -5,12 +5,13 @@ class BankAccount
 {
     static void Main()
     {
-        /* The strict "solution" is commented out in this section.
-         * The uncommented code is for fun, run it if you have the time.
-         * All variables except the balance are strings, I think it's best.
-         */
-
-
+        /*
+        Abank account has a holder name (first name, middle name and last name), 
+        available amount of money (balance), bank name, IBAN, BIC code and 3 credit 
+        card numbers associated with the account. Declare the variables needed to keep 
+        the information for a single bank account using the appropriate data types and 
+        descriptive names.
+        */
 
         Console.CursorVisible = false;
         Console.WriteLine(
