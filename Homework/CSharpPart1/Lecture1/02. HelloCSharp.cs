4b2a500901aac5aab25670afc292ef1b31501920
georgiwe <georgiwe@gmail.com>
2013-11-03 15:59:06 +0200

    Create 02. HelloCSharp.cs

---
+++ Homework/CSharpPart1/Lecture1/02. HelloCSharp.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Text;
+
+class HelloCSharp
+{
+    static void Main()
+    {
+        //Create, compile and run a “Hello C#” console application.
+
+        Console.WriteLine("Hello C#\n");
+    }
+}
