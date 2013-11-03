09676ceea98f0310da2e634a93621163d75f5a8e
georgiwe <georgiwe@gmail.com>
2013-11-03 15:52:21 +0200

    Create 02. DoubleFloat.cs

---
+++ Homework/CSharpPart1/Lecture2/02. DoubleFloat.cs
@@ -0,0 +1,17 @@
+using System;
+
+class DoubleFloat
+{
+    static void Main()
+    {
+        /*Which of the following values can be assigned 
+         * to a variable of type float and which to a variable of type double: 
+         * 34.567839023, 12.345, 8923.1234857, 3456.091?
+         */
+
+        double doubleNum1 = 34.567839023;
+        float floatNum1 = 12.345f;
+        double doubleNum2 = 8923.1234857;
+        float floatNum2 = 3456.091f;
+    }
+}
