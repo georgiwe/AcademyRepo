24f1274fc2e360bca57a15917c342f83f4685cb0
georgiwe <georgiwe@gmail.com>
2013-11-03 15:53:01 +0200

    Create HexadecimalInt.cs

---
+++ Homework/CSharpPart1/Lecture2/HexadecimalInt.cs
@@ -0,0 +1,13 @@
+using System;
+
+class HexadecimalInt
+{
+    static void Main()
+    {
+        //Declare an integer variable and assign it with the value 254 
+        //in hexadecimal format. Use Windows Calculator to find 
+        //its hexadecimal representation.
+
+        int numberInHex = 0xFE;
+    }
+}
