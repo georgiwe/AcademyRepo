79bc5e8cb5b7d53da1a1b476e588e3a67d787033
georgiwe <georgiwe@gmail.com>
2013-11-03 15:59:43 +0200

    Create 03. PrintMahName.cs

---
+++ Homework/CSharpPart1/Lecture1/03. PrintMahName.cs
@@ -0,0 +1,10 @@
+using System;
+
+class PrintMahName
+{
+    static void Main()
+    {
+        //Modify the application to print your name.
+        Console.WriteLine("My name is \"Gladiator\"!\n");
+    }
+}
