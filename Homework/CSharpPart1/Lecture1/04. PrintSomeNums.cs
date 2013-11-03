63241424dc9e5814859df3fc224543b6675bb5ed
georgiwe <georgiwe@gmail.com>
2013-11-03 15:59:57 +0200

    Create 04. PrintSomeNums.cs

---
+++ Homework/CSharpPart1/Lecture1/04. PrintSomeNums.cs
@@ -0,0 +1,13 @@
+using System;
+
+class PrintSomeNums
+{
+    static void Main()
+    {
+        //Write a program to print the numbers 1, 101 and 1001.
+
+        Console.Write("1, ");
+        Console.Write("101, ");
+        Console.WriteLine("1001");
+    }
+}
