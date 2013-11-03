61898c27595b95f3dcefa51a8b716a87539becca
georgiwe <georgiwe@gmail.com>
2013-11-03 16:00:10 +0200

    Create 06. FirstAndLastName.cs

---
+++ Homework/CSharpPart1/Lecture1/06. FirstAndLastName.cs
@@ -0,0 +1,15 @@
+using System;
+
+class FirstLast
+{
+    static void Main()
+    {
+        //Create console application that prints your first and last name.
+
+        Console.WriteLine("My name is Maximus Decimus Meridius.\n");
+        Console.WriteLine("Commander of the armies of the north. General of the Felix Legions.\nLoyal servant to the true emperor, Marcus Aurelius!");
+        Console.WriteLine("Father to a murdered son, husband to a murdered wife, \nand I will have my vengeance - in this life, or the next.\n");
+        Console.WriteLine("...\n");
+        Console.WriteLine("Also, C# is NEAT!\n");
+    }
+}
