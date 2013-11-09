b652b8effa5e7fdcade430913e25b956ab594dd6
georgiwe <georgiwe@gmail.com>
2013-11-10 01:10:11 +0200

    Update 14. BitExchange.cs

--- Homework/CSharpPart1/Lecture3/14. BitExchange.cs
+++ Homework/CSharpPart1/Lecture3/14. BitExchange.cs
@@ -26,7 +26,7 @@ class BitExchange
         Console.WriteLine("\n\n{0} {1,14} {2, 4} {3, 4} {4, 4} {5, 4} {6, 4} {7, 3} {8, 4}","", 32, 28, 24, 20, 16, 12, 8, 4);
         Console.WriteLine("{0,-10} ={1, 32}", number, numberBitwiseSorted);
 
-        for (int i = -1; i < numberOfBitsToChange; i++)
+        for (int i = -1; i < numberOfBitsToChange - 1; i++)
         {
             int mask = 1 << frontStartingBit + i;
             mask = resultNumber & mask;
