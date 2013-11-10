e9669d814f4fce58d3ffa7238ff8da4ba8198e3d
Georgi Prodanov <georgiwe@gmail.com>
2013-11-10 15:41:46 +0200

    Commit Message

---
+++ Misc/BatGoikoTower2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class BatGoikoTower2
+{
+    static void Main()
+    {
+        int height = int.Parse(Console.ReadLine());
+        int[] crossbeam = new int[height];
+        int step = 2;
+
+        crossbeam[0] = 1;
+        for (int i = 1; i < height/2; i++)
+        {
+            crossbeam[i] = crossbeam[i-1] + step;
+            step++;
+        }
+
+        int outerDots = height - 1;
+        int innerDots = 0;
+        step = 0;
+
+        for (int i = 0; i < height; i++)
+        {
+            Console.Write(new string('.', outerDots));
+            Console.Write("/");
+            if (i == crossbeam[step]) { Console.Write(new string('-', innerDots)); step++; }
+            else Console.Write(new string('.', innerDots));
+            Console.Write("\\");
+            Console.WriteLine(new string('.', outerDots));
+
+            outerDots--;
+            innerDots += 2;
+        }
+    }
+}
