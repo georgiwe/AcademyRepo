00e81ead32e1b031dd29b9079427d9f75ce0e74e
georgiwe <georgiwe@gmail.com>
2013-11-14 02:13:38 +0200

    Update 11. FallingRocks.cs

--- Homework/CSharpPart1/Lecture4/11. FallingRocks.cs
+++ Homework/CSharpPart1/Lecture4/11. FallingRocks.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading;
+using System.Text;
 
 class Program
 {
@@ -18,35 +19,56 @@ class Program
         int score = 0;
         int difficulty = 1;
         int step = 0;
+        int step2 = 0;
         int left = 10;
+        byte lives = 3;
+        bool lightshow = false;
 
         for (int i = 0; i < screen.Length; i++) screen[i] = new string(' ', 20);
 
-        //string[] colorNames = ConsoleColor.GetNames(typeof(ConsoleColor));
+        string[] colorNames = ConsoleColor.GetNames(typeof(ConsoleColor));
         char[] rocks = new char[] { '@', '^', '*', '&', '+', '%', '$', '#', '!', '.', ';' };
 
-        Console.WriteLine("- Use the Left and Right arrow keys to move sideways.\n- Use '+' and '-' to adjust game speed.");
-        Console.WriteLine("- Press \"P\" to pause.");
+        Console.WriteLine("- Use the Left and Right arrow keys to move sideways.\n\n- Use '+' and '-' to adjust game speed.");
+        Console.WriteLine("\n- Press 'P' to pause.");
+        Console.WriteLine("- Press 'Delete' to toggle \"Lightshow mode\"");
+        Console.WriteLine("\n\n\nIf you score 11 000 points - you win.");
         Console.WriteLine("\nPress any key to start game.");
         Console.ReadKey();
 
         while (true)
         {
-            if (score - step == 1000) { difficulty++; step += 1000; } // increases difficulty until 10 000 points
+            byte livesCheck = lives;
+            
 
             Console.Clear();
             if (counter % speed == 0)
             {
                 for (int i = 0; i < screen[screen.Length - 1].Length - 1; i++)
                 {
-                    if (screen[screen.Length - 1][i] != ' ' && (i >= left && i < left + 3))
+                    if ((screen[screen.Length - 1][i] != ' ' && (i >= left && i < left + 3)) || (score >= 11000))
                     {
                         gameOver = true;
+
+                        lives--;
                         break;
                     }
-                    else if (screen[screen.Length - 1][i] != ' ') score += 10;
+                    else if (screen[screen.Length - 1][i] != ' ')
+                    {
+                        score += 100;
+
+                        if (score - step2 == 1000 && score != 0)
+                        {
+                            lives++;
+                            step2 = score;
+                        }
+                        // increases difficulty until difficulty lvl 6
+                        if (score - step == 2000 && score != 0 && difficulty <= 6) 
+                            { difficulty++; step = score; counter = 0; } 
+                    }
                 }
-                if (gameOver) break;
+
+                if ((gameOver && lives == 0) || (score >= 11000)) break;
 
                 // pieces fall down
                 for (int i = screen.Length - 1; i >= 1; i--)
@@ -55,31 +77,33 @@ class Program
                 }
 
                 // random number of pieces generated depending on difficulty level
-                screen[0] = "";
+                screen[0] = new string(' ', 20);
                 int count = rnd.Next(1, difficulty + 1);
                 for (int i = 1; i <= count; i++)
-                {
-                    if (screen[0].Length < 19)
-                    {
-                        int indent = rnd.Next(19 / i);
-                        screen[0] += new string(' ', indent) + new string(rocks[rnd.Next(rocks.Length)], rnd.Next(1,difficulty));
-                    }
+                 {
+                    char[] tempChar = screen[0].ToCharArray();
+                    tempChar[rnd.Next(0, tempChar.Length)] = rocks[rnd.Next(rocks.Length)];
+                    screen[0] = (new string(tempChar)).PadRight(20, ' ');
                 }
-                screen[0] = screen[0].PadRight(20, ' ');
             }
 
             // prints the pieces after they have dropped one line
             for (int i = 0; i < screen.Length; i++)
             {
-                //ConsoleColor color = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), colorNames[rnd.Next(colorNames.Length)]);
-                //Console.ForegroundColor = color;
-                Console.WriteLine(screen[i]);
+                if (score >= 11000 || lightshow)
+                {
+                    ConsoleColor color = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), colorNames[rnd.Next(colorNames.Length)]);
+                    Console.ForegroundColor = color;
+                    Console.WriteLine(screen[i]);
+                }
+                else Console.WriteLine(screen[i]);
             }
 
             // prints the dwarf and score
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(new string(' ', left) + dwarf);
-            Console.WriteLine("Score: {0}", score);
-
+            Console.WriteLine("Score: {0}   Lives: {1}", score, lives);
+            Console.ResetColor();
             // takes and conceals keyboard input
             Console.ForegroundColor = ConsoleColor.Black;
             if (Console.KeyAvailable == true)
@@ -104,10 +128,32 @@ class Program
                 else if (command.Key == ConsoleKey.P)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Game paused. Press any key to continue...");
+                    Console.WriteLine("\bGame paused. Press any key to continue...");
                     Console.ResetColor();
                     while (!Console.KeyAvailable) { }
                 }
+                else if (command.Key == ConsoleKey.L) lives++;
+                else if (command.Key == ConsoleKey.Delete) lightshow = !lightshow;
+            }
+
+            if (lives < livesCheck)
+            {
+                Console.SetCursorPosition(30, 10);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You have lost a life!");
+                Console.SetCursorPosition(30, 11);
+                Console.WriteLine("You have {0} lives left.", lives);
+                Console.SetCursorPosition(30, 12);
+                Console.WriteLine();
+                Console.SetCursorPosition(30, 13);
+                Console.WriteLine("Press Enter to continue.");
+                Console.ForegroundColor = ConsoleColor.Black;
+                while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.SetCursorPosition(5, 10);
+                Console.WriteLine("Get ready!");
+                Console.ResetColor();
+                Thread.Sleep(950);
             }
 
             Console.ResetColor();
@@ -115,9 +161,21 @@ class Program
             counter++;
         }
 
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("\n\n\n\n\n\n\n{0, 34}\n", "Game Over");
-        Console.WriteLine("{0, 32}{1}{2}","Your score is ", score, " points!");
-        Console.ReadLine();
+        if (score < 11000)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n\n\n\n\n\n\n{0, 33}\n", "Game Over");
+            Console.WriteLine("{0, 29}{1}{2}", "You scored ", score, " points!");
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n\n\n\n\n\n\n{0, 33}\n", "YOU WON!!");
+            Console.WriteLine("{0, 28}{1}{2}", "You scored ", score, " points!");
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
     }
 }
