ac0400df05fedc1aaae990033502dce2c6a92885
georgiwe <georgiwe@gmail.com>
2013-12-12 22:42:16 +0200

    Create 11.IndexOfElement.cs

---
+++ Homework/CSharpPart2/01.Arrays/11.IndexOfElement.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+
+class IndexOfElement
+{
+    static void Main()
+    {
+        Console.Write("How many elements in the array: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Element {0} = ", i);
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        Array.Sort(numbers);
+        Console.WriteLine("\nThe sorted array is: \n");
+        for (int i = 0; i < numbers.Length; i++)
+            Console.WriteLine("array[{0}] = {1}", i, numbers[i]);
+
+        Console.Write("\nWe are looking for the index of: ");
+        int numWeLookFor = int.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+        int firstIndex = 0;
+        int lastIndex = numbers.Length - 1;
+        int index = (firstIndex + lastIndex) / 2;
+
+        
+        while (index != firstIndex && index != lastIndex)
+        {
+            index = (firstIndex + lastIndex) / 2;
+
+            if (numWeLookFor > numbers[index]) firstIndex = index + 1;
+            else if (numWeLookFor < numbers[index]) lastIndex = index - 1;
+            else
+            {
+                Console.WriteLine(new string('=', 15) + "\n");
+                int newIndex = index;
+                var indices = new List<int>();
+                int counter = 0;
+
+                while (newIndex >= 0 && numbers[newIndex] == numWeLookFor)
+                {
+                    indices.Add(newIndex);
+                    counter++;
+                    newIndex--;
+                }
+
+                newIndex = index + 1;
+                while ((newIndex <= (numbers.Length - 1)) && numbers[newIndex] == numWeLookFor)
+                {
+                    indices.Add(newIndex);
+                    counter++;
+                    newIndex++;
+                }
+
+                if (counter != 1)
+                {
+                    Console.WriteLine("Found {0} occurance of the number \"{1}\".", counter, numWeLookFor);
+                    Console.Write("Their indices are: ");
+
+                    indices.Sort();
+
+                    for (int i = 0; i < counter; i++)
+                    {
+                        if (counter == 1) Console.WriteLine("");
+                        else if (i < counter - 1) Console.Write(indices[i] + ", ");
+                        else Console.WriteLine("\b\b and " + indices[i]);
+                    }
+                }
+                else
+                {
+                    Console.Write("Found just one occurance of the number \"{0}\" and ", numWeLookFor);
+                    Console.WriteLine("its index is {0}.", index);
+                }
+
+                Console.WriteLine();
+                return;
+            }
+        }
+
+        Console.WriteLine("{0} is not present in the given array.\n", numWeLookFor);
+    }
+}
