a46f3cb88ca3007f5a7e291250ec788f7bbd8ba4
georgiwe <georgiwe@gmail.com>
2013-12-16 15:56:58 +0200

    Create 19.Permutations.cs

---
+++ Homework/CSharpPart2/01.Arrays/19.Permutations.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+
+class Permutations
+{
+    static void Work(List<int> elements, string toPrint = "")
+     {
+        if (toPrint.Length >= elements.Count) return;
+
+        for (int i = 0; i < elements.Count; i++)
+        {
+            if (toPrint.Contains(elements[i].ToString()) == false) 
+            {
+                toPrint += elements[i];
+                Work(elements, toPrint);
+
+                if (toPrint.Length == elements.Count) Console.WriteLine(toPrint);
+                else if (toPrint.Length > 0) toPrint = toPrint.Substring(0, toPrint.Length - 1);
+            }
+        }
+    }
+
+    static void Main()
+    {
+        int n = 3;
+
+        var elements = new List<int>();
+        for (int i = 1; i <= n; i++) elements.Add(i);
+
+        Work(elements);
+    } 
+}
