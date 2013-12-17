7f2c859e2c4ec5b9ab4d81ff59e1c75c0169ce21
georgiwe <georgiwe@gmail.com>
2013-12-17 10:14:13 +0200

    Create 20.Variations.cs

---
+++ Homework/CSharpPart2/01.Arrays/20.Variations.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+class Permutations
+{
+    static void Work(List<int> elements, int k, string toPrint = "")
+    {
+        if (toPrint.Length >= k) return;
+
+        for (int i = 0; i < elements.Count; i++)
+        {
+            toPrint += elements[i];
+            Work(elements, k, toPrint);
+
+            if (toPrint.Length == k) Console.WriteLine(toPrint);
+            if (toPrint.Length > 0) toPrint = toPrint.Substring(0, toPrint.Length - 1);
+        }
+    }
+
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int k = int.Parse(Console.ReadLine());
+
+        var elements = new List<int>();
+        for (int i = 1; i <= n; i++) elements.Add(i);
+
+        Work(elements, k);
+    }
+}
