e1cf4f8a9d6f1d5c34b3bff5c12144287ebb4060
georgiwe <georgiwe@gmail.com>
2013-12-20 18:45:39 +0200

    Create 05.SortingStringArrays.cs

---
+++ Homework/CSharpPart2/02.MultidimensionalArrays/05.SortingStringArrays.cs
@@ -0,0 +1,38 @@
+// You are given an array of strings. Write a method that sorts the array 
+// by the length of its elements (the number of characters composing them).
+
+using System;
+using System.Collections.Generic;
+
+class SortingStringArrays
+{
+    static string[] Qsort(string[] arr)
+    {
+        if (arr.Length <= 1) return arr;
+
+        string pivot = arr[arr.Length - 1];
+
+        var shorter = new List<string>();
+        var longer = new List<string>();
+
+        for (int i = 0; i < arr.Length - 1; i++)
+            if (arr[i].Length < pivot.Length) shorter.Add(arr[i]);
+            else longer.Add(arr[i]);
+
+        shorter = new List<string>(Qsort(shorter.ToArray()));
+        longer = new List<string>(Qsort(longer.ToArray()));
+
+        shorter.Add(pivot);
+        shorter.AddRange(longer);
+
+        return shorter.ToArray();
+    }
+
+    static void Main()
+    {
+        string[] stringsBro = { "Ariana", "Heineken", "Zagorka", "Shumensko",
+                                "Bruhaha", "Astika", "Carlsberg", "Staropramen"};
+
+        stringsBro = Qsort(stringsBro);
+    }
+}
