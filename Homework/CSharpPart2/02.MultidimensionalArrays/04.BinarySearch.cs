ebd96de332d265ffbd19a1625152050966087863
georgiwe <georgiwe@gmail.com>
2013-12-21 17:58:25 +0200

    Update 04.BinarySearch.cs

--- Homework/CSharpPart2/02.MultidimensionalArrays/04.BinarySearch.cs
+++ Homework/CSharpPart2/02.MultidimensionalArrays/04.BinarySearch.cs
@@ -4,19 +4,23 @@ class BinarySearch
 {
     static void Main()
     {
+        Console.Write("Enter N, followed by the elements: ");
         int n = int.Parse(Console.ReadLine());
         int[] integers = new int[n];
 
         for (int i = 0; i < n; i++) integers[i] = int.Parse(Console.ReadLine());
-
+        Console.Write("Enter K: ");
         int k = int.Parse(Console.ReadLine());
 
         Array.Sort(integers);
 
-        var resultIndex = Array.BinarySearch(integers, k);
-
-        while ((resultIndex = Array.BinarySearch(integers, k)) < 0) k--;
-
-        Console.WriteLine(integers[resultIndex]);
+        if (k < integers[0]) { Console.WriteLine("K is smaller than all the elements of the array."); return; }
+        if (k >= integers[n - 1]) { Console.WriteLine("The largest number is {0} and its index is {1}", integers[n - 1], n - 1); return; }
+        // the check for k >= than the last (biggest) numbet of the array saves the binary search
+        var index = Array.BinarySearch(integers, k);
+        
+        if (index < -1) index = ~index - 1;
+        
+        Console.WriteLine("The largest number <= K is {0} and its index is {1}.", integers[index], index);
     }
 }
