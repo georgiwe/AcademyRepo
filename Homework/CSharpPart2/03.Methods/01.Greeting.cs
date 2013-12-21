f960dd8098a203a23ee570ae48ced26aab0a12a2
georgiwe <georgiwe@gmail.com>
2013-12-21 22:11:48 +0200

    Update 01.Greeting.cs

--- Homework/CSharpPart2/03.Methods/01.Greeting.cs
+++ Homework/CSharpPart2/03.Methods/01.Greeting.cs
@@ -6,20 +6,20 @@ using System.Globalization;
 
 class HelloNameLOL
 {
-    static string Greeting(string name)
+    static void Greeting(string name)
     {
         TextInfo ti = new CultureInfo("en-US").TextInfo;
 
         foreach (var character in name)
             if (!char.IsLetter(character) && !char.IsWhiteSpace(character)) 
-                return "Try entering a proper name and then come back!";
+                { Console.WriteLine("Try entering a proper name and then come back!"); return; }
 
-        return "Hello, " + ti.ToTitleCase(name) + "!";
+        Console.WriteLine("\nHello, {0}!\n", ti.ToTitleCase(name));
     }
 
     static void Main()
     {
         Console.WriteLine("Enter your name, mofo! And no digits! ");
-        Console.WriteLine(Greeting(Console.ReadLine().ToLower()));
+        Greeting(Console.ReadLine());
     }
 }
