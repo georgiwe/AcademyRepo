68737fe50e92f04233d4ee6402eeea0b3c7234e2
georgiwe <georgiwe@gmail.com>
2013-12-21 16:15:06 +0200

    Update 07.AreaOfNeighbours.cs

--- Homework/CSharpPart2/02.MultidimensionalArrays/07.AreaOfNeighbours.cs
+++ Homework/CSharpPart2/02.MultidimensionalArrays/07.AreaOfNeighbours.cs
@@ -48,14 +48,12 @@ class AreaOfNeighbours
 
     static void Main()
     {
-        int?[,] arr =
-        {
+        int?[,] arr = {
             {1, 3, 2, 2, 2, 4},
             {3, 3, 3, 2, 4, 4},
             {4, 3, 1, 2, 3, 3},
             {4, 3, 1, 3, 3, 1},
-            {4, 3, 3, 3, 1, 1},
-        };
+            {4, 3, 3, 3, 1, 1}, };
 
         maxElement = arr[0, 0];
 
