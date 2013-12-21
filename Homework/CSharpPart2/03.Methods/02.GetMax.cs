7abc508c184091c356c7ad12685b95d50a0e2979
georgiwe <georgiwe@gmail.com>
2013-12-21 22:44:25 +0200

    Create 02.GetMax.cs

---
+++ Homework/CSharpPart2/03.Methods/02.GetMax.cs
@@ -0,0 +1,30 @@
+// Write a method GetMax() with two parameters that returns the bigger of two integers. 
+// Write a program that reads 3 integers from the console and prints the biggest of them 
+// using the method GetMax()
+
+using System;
+using System.Collections.Generic;
+
+class GetDaMAX
+{
+    static int? GetMax(List<int> numbers)
+    {
+        if (numbers.Count == 0) { Console.WriteLine("You didnt enter anything!"); return null; }
+
+        numbers.Sort();
+        return numbers[numbers.Count - 1];
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Enter as many numbers as you wish. Enter \"Quit\" when you are done.");
+        
+        int input = 0;
+        var numbers = new List<int>();
+        
+        while (int.TryParse(Console.ReadLine(), out input)) numbers.Add(input);
+
+        int? theMAX = GetMax(numbers);
+        if (theMAX != null) Console.WriteLine("The max is {0}.", theMAX);
+    }
+}
