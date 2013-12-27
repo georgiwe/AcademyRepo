9f95b7da120b2ae0f87cfc9932fb11d33086c526
georgiwe <georgiwe@gmail.com>
2013-12-27 19:45:01 +0200

    Create 07.BaseToBase.cs

---
+++ Homework/CSharpPart2/04.NumeralSystems/07.BaseToBase.cs
@@ -0,0 +1,46 @@
+// Write a program to convert from any numeral system of given base s 
+// to any other numeral system of base d (2 ≤ s, d ≤  16).
+
+using System;
+
+class BaseToBase
+{
+    static void Main()
+    {
+        Console.Write("Enter the number you want converted: ");
+        string number = Console.ReadLine();
+        Console.Write("Enter its initial base (2 - 36): ");
+        int fromBase = int.Parse(Console.ReadLine());
+        Console.Write("Enter the desired output base (2 - 36): ");
+        int toBase = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("The result is: \"{0}\".", ConvFromBaseToBase(number.TrimStart('0').ToUpper(), fromBase, toBase));
+        Console.WriteLine();
+    }
+
+    static string ConvFromBaseToBase(string number, int fromBase, int toBase)
+    {
+        if (fromBase == toBase) return number;
+
+        string result = "";
+        long decResult = 0;
+        char[] chars = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+                         'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', };
+
+        for (int i = number.Length - 1; i >= 0; i--)
+            if (number[i] >= 'A') decResult += (number[i] - 55) * (long)BinToDec.Pow(fromBase, number.Length - 1 - i);
+            else decResult += (number[i] - 48) * (long)BinToDec.Pow(fromBase, number.Length - 1 - i);
+
+        do
+        {
+            long remainder = decResult % toBase;
+
+            if (remainder > 9) result = chars[remainder - 10] + result;
+            else result = remainder + result;
+
+            decResult /= toBase;
+        } while (decResult != 0);
+
+        return result;
+    }
+}
