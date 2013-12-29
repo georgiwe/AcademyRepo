f14d18fdcd71f1bb77a7d4e52a47096bc91a4f3c
georgiwe <georgiwe@gmail.com>
2013-12-29 18:09:10 +0200

    Update 05.Workdays.cs

--- Homework/CSharpPart2/05.ClassesAndObjects/05.Workdays.cs
+++ Homework/CSharpPart2/05.ClassesAndObjects/05.Workdays.cs
@@ -23,11 +23,12 @@ class Workdays
         if (start > end) { Console.WriteLine("Erroneous input!"); return; }
 
         DateTime[] holidays = {
-        new DateTime(start.Year,  1,  1),
-        new DateTime(start.Year,  3,  3),
-        new DateTime(start.Year,  5,  6),
-        new DateTime(start.Year,  5, 24),
-        new DateTime(start.Year, 10,  9), }; // My birthday, obv. 
+        new DateTime(start.Year - 1, 12, 31),
+        new DateTime(start.Year - 1,  1,  1),
+        new DateTime(start.Year - 1,  3,  3),
+        new DateTime(start.Year - 1,  5,  6),
+        new DateTime(start.Year - 1,  5, 24),
+        new DateTime(start.Year - 1, 10,  9), }; // My birthday, obv. 
                                                                                 // If it is Sat/Sun, we
         if (start.DayOfWeek == DayOfWeek.Saturday) start = start.AddDays(2);    // start counting from the
         else if (start.DayOfWeek == DayOfWeek.Sunday) start = start.AddDays(1); // first upcoming Monday.
@@ -35,7 +36,7 @@ class Workdays
         int subtractedDays = 0;
         if (end.DayOfWeek == DayOfWeek.Sunday) // We cout only full days, so no need to remove Saturdays.
         {                                      // We skip Saturdays with the 00:00 and on Mondays,
-            end = end.AddDays(-1); // Subtracts one day.        // we have already removed the two weekend days.
+            end = end.AddDays(-1); // Subtracts one day.        // we have already removed the two days.
             subtractedDays += 1;   // We remember how many days we subtracted,
         }                          // in order to display the right date in the end.
 
@@ -46,13 +47,15 @@ class Workdays
 
         for (int y = 0; y <= end.Year - start.Year; y++)
         {
+            Console.WriteLine(y + start.Year);
+
             for (int i = 0; i < holidays.Length; i++)
             {
                 holidays[i] = new DateTime(holidays[i].Year + 1, holidays[i].Month, holidays[i].Day);
 
                 if (holidays[i] <= end && holidays[i] >= start &&
                     holidays[i].DayOfWeek != DayOfWeek.Saturday && holidays[i].DayOfWeek != DayOfWeek.Sunday)
-                        Console.Write("\n{0:MMM dd, yyyy} has been excluded as a holiday.", holidays[i], resultDays--);
+                        Console.WriteLine("{0:MMM dd, yyyy} has been excluded as a holiday.", holidays[i], resultDays--);
             }
 
             Console.WriteLine();
