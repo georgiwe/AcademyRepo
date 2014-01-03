5b56e1cfa5c10d4ff6756dd89ae451d68d9b2f8d
georgiwe <georgiwe@gmail.com>
2014-01-03 09:56:32 +0200

    Create 07.Replace.cs

---
+++ Homework/CSharpPart2/06.TextFiles/07.Replace.cs
@@ -0,0 +1,25 @@
+// Write a program that replaces all occurrences of the substring "start" 
+// with the substring "finish" in a text file. Ensure it will work with large files (e.g. 100 MB)
+
+using System;
+using System.IO;
+
+class Replace
+{
+    static void Main()
+    {
+        StreamReader reader = new StreamReader("input.txt");
+        StreamWriter writer = new StreamWriter("output.txt");
+
+        string line = reader.ReadLine();
+
+        while (line != null)
+        {
+            writer.WriteLine(line.Replace("start", "finish"));
+            line = reader.ReadLine();
+        }
+
+        reader.Dispose();
+        writer.Dispose();
+    }
+}
