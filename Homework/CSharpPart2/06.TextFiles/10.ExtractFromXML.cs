4fdd8ab05553b50ea8820567a21f7c5a655fe320
georgiwe <georgiwe@gmail.com>
2014-01-04 18:11:11 +0200

    Create 10.ExtractFromXML.cs

---
+++ Homework/CSharpPart2/06.TextFiles/10.ExtractFromXML.cs
@@ -0,0 +1,17 @@
+// Write a program that extracts from given XML file all the text without the tags
+
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class ExtractFromXML
+{
+    static void Main()
+    {
+        string text = File.ReadAllText("xml.txt");
+
+        MatchCollection matches = Regex.Matches(text, @">\s*\w+\s*<", RegexOptions.IgnoreCase);
+
+        foreach (Match match in matches) Console.WriteLine(match.Value.Trim(' ', '<', '>'));
+    }
+}
