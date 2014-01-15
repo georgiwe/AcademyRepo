5b0cf3b2458cee00366209d732e51b2082737ae2
georgiwe <georgiwe@gmail.com>
2014-01-15 20:03:35 +0200

    Update TheyAreGreen.cs

--- Misc/CSharp2ExamPrep/TheyAreGreen.cs
+++ Misc/CSharp2ExamPrep/TheyAreGreen.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
 
-class Program
+class TheyAreGreen
 {
     static int n = 0;
     static int wordCount = 0;
@@ -11,47 +11,45 @@ class Program
     static void Main()
     {
         n = int.Parse(Console.ReadLine());
-
         char[] allLetts = new char[n];
 
         for (int i = 0; i < n; i++)
             allLetts[i] = char.Parse(Console.ReadLine());
 
         var letts = new List<char>();
-        
+
         for (int i = 0; i < allLetts.Length; i++)
             if (!letts.Contains(allLetts[i])) letts.Add(allLetts[i]);
 
-        //if (letts.Count == n) { Console.WriteLine(Fact(n)); return; }
+        if (letts.Count == n) { Console.WriteLine(Fact(n)); return; }
 
         var counts = new List<int>(letts.Count);
-        string allStr = String.Join("", allLetts);
+        var allStr = new string(allLetts);
 
         for (int i = 0; i < letts.Count; i++)
             counts.Add(Regex.Matches(allStr, letts[i].ToString()).Count);
 
-        var soFar = new char[n];
-
-        Solve(letts, counts, soFar);
+        var sb = new StringBuilder();
+        Solve(letts, counts, sb);
 
         Console.WriteLine(wordCount);
     }
 
-    static void Solve(List<char> letts, List<int> counts, char[] soFar, int index = 0)
+    static void Solve(List<char> letts, List<int> counts, StringBuilder soFar)
     {
-        if (index == n) { wordCount++; return; }
+        if (soFar.Length == n) { wordCount++; return; }
 
         for (int i = 0; i < letts.Count; i++)
         {
-            if ((index == 0 || soFar[index - 1] != letts[i]) && counts[i] > 0)
+            if (counts[i] > 0 && (soFar.Length == 0 || soFar[soFar.Length - 1] != letts[i]))
             {
-                soFar[index] = letts[i];
+                soFar.Append(letts[i]);
                 counts[i]--;
 
-                Solve(letts, counts, soFar, index + 1);
+                Solve(letts, counts, soFar);
 
                 counts[i]++;
-                soFar[index] = '0';
+                soFar.Length--;
             }
         }
     }
